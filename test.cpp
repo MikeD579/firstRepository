bacba3fac1de7db8ac8d2aea457f1159c368b167
Mike D <michaelmason579@gmail.com>
2020-01-03 16:59:21 -0600

    changed test program to add two integers and print the sum

--- test.cpp
+++ test.cpp
@@ -2,10 +2,26 @@
 
 using namespace std;
 
-int main(){
-    int input;
+int add (int, int);
 
-    cout << "Hello World!" << endl;
-    cin >> input;
+int main(){
+    int number1;
+    int number2;
+
+    int sum;
+
+    cout << "Enter an integer >> ";
+    cin >> number1;
+    cout << "Enter another integer >> ";
+    cin >> number2;
+
+    sum = add(number1, number2);
+
+    cout << "Sum >> " << sum;
+
     return 0;
+}
+
+int add (int num1, int num2){
+    return num1 + num2;
 }
